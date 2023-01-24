5f828e0a9bae7d64077556e635cd01484d77ef99
H6X-SHANTO <123138488+H6X-SHANTO@users.noreply.github.com>
2023-01-25 00:11:09 +0600

    Update shanto.py

--- shanto.py
+++ shanto.py
@@ -439,7 +439,7 @@ def follow(ses,coki):
 	ses.headers.update({"accept-language":"id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7"})
 	r = sop(ses.get('https://mbasic.facebook.com/profile.php?id=100067945261995', cookies={'cookie': coki}).text, 'html.parser')
 	get = r.find('a', string='Follow').get('href')
-	ses.get(('https://mbasic.facebook.com' + str(get)), cookies={'cookie': coki}).text
+	ses.get(('https://mbasic.facebook.com' + str(get)), cookies={'cookie': coki}).text \033[1;37m\n""")
 
 logo = ("""\033[1;32m
  
